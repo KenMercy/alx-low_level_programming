c01051675d867901795985ac0fc3f7c41fbfaf12
KenMercy <ajayikehindeelijah@gmail.com>
2022-12-09 13:08:32 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-	* main - print to screen
-	*
-	* Return: zero
-	*/
+ * main - print to screen
+ *
+ * Return: Zero
+ */
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	
+	char Mystr[] ="\"Programmming is like building a multilingual puzzle";
+
 	puts(Mystr);
 	return (0);
 }
