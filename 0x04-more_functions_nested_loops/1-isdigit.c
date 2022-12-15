c20cb81de052e12dc1ffcabc1b8c1d24401ee38e
KenMercy <ajayikehindeelijah@gmail.com>
2022-12-15 23:09:07 +0100

    checks for a digit (0 through 9)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isupper - Checks if c is an uppercase letter
+ * _isdigit - Checks if c is an ASCII digit
  * @c: ASCII value to be evaluated
  * Return: 1 if uppercase else 0
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
 }
